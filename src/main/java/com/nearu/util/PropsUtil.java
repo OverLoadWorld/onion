469880f331582a6962b9e747292a6b0e5cb929bb
Howard <hizzyzzh@hotmail.com>
2018-09-03 00:53:37 +0800

    util

--- src/main/java/com/nearu/util/PropsUtil.java
+++ src/main/java/com/nearu/util/PropsUtil.java
@@ -1,10 +1,79 @@
 package com.nearu.util;
 
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class PropsUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(PropsUtil.class);
 
+    public static Properties loadProps(String fileName){
+        Properties properties = null;
+        InputStream is = null;
+        try {
+
+            is = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+            if (is == null) {
+                throw new FileNotFoundException(fileName + " file not found");
+            }
+            properties = new Properties();
+            properties.load(is);
+        } catch (Exception e) {
+            logger.error("load properties file failed.", e);
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    logger.error("close input stream failure", e);
+                }
+            }
+        }
+        return properties;
+    }
+
+    public static String getString(Properties props, String key) {
+        return getString(props, key, "");
+    }
+
+    public static String getString(Properties props, String key, String defaultValue) {
+        String value = defaultValue;
+        if(props.containsKey(key)){
+            value = props.getProperty(key);
+        }
+        return value;
+    }
+
+    public static int getInt(Properties props, String key){
+        return getInt(props, key, 0);
+    }
+
+    public static int getInt(Properties props, String key, int defaultValue){
+        int value = defaultValue;
+        if (props.containsKey(key)) {
+            value = NumberUtils.toInt(props.getProperty(key), defaultValue);
+        }
+        return value;
+    }
+
+    public static boolean getBoolean(Properties props, String key) {
+        return getBoolean(props, key, false);
+    }
+
+    public static boolean getBoolean(Properties props, String key, boolean defaultValue) {
+        boolean value = defaultValue;
+        if (props.containsKey(key)) {
+            value = BooleanUtils.toBoolean(props.getProperty(key));
+        }
+        return value;
+
+    }
 }
